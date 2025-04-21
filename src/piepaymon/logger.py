862ef52ac620ca5512d1e83ad24a21c611575826
VipulOG <90324465+VipulOG@users.noreply.github.com>
2025-04-21 14:09:21 +0530

    style(logger): use default colors for console log formatter

--- src/piepaymon/logger.py
+++ src/piepaymon/logger.py
@@ -30,13 +30,6 @@ def setup_logger():
     console_formatter = ColoredFormatter(
         "%(asctime)s - %(name)s - %(log_color)s%(levelname)s%(reset)s - %(message)s",
         datefmt="%H:%M:%S",
-        log_colors={
-            "DEBUG": "cyan",
-            "INFO": "green",
-            "WARNING": "yellow",
-            "ERROR": "red",
-            "CRITICAL": "bold_red",
-        },
     )
 
     console_handler = logging.StreamHandler()
